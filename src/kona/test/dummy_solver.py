03f4bcb72aacd7b9e3031d9cb5478323cfa544ea
Alp Dener <alp@dener.me>
2015-04-08 15:08:20 -0400

    ongoing work on dummy solver

--- src/kona/test/dummy_solver.py
+++ src/kona/test/dummy_solver.py
@@ -1,18 +1,69 @@
+import numpy
+
 from kona.user import UserSolver
 from kona.user import BaseVector
 
 class DummySolver(UserSolver):
 
     def eval_obj(self, at_design, at_state):
-        if isinstance(at_design, BaseVector) and isinstance(at_state, BaseVector):
-            return 1.
-        else:
-            return 0.
+        return numpy.sum(at_design.data) + numpy.sum(at_state.data)
 
-    def eval_residual(self, at_design, at_state, result):
+    def eval_residual(self, at_design, at_state, store_here):
+        store_here.data[:] = numpy.sum(at_design.data) + at_state.data[:]
+
+    def eval_ceq(self, at_design, at_state, store_here):
+        store_here.data[:] = self.eval_obj(at_design, at_state)
+
+    def multiply_dRdX(self, at_design, at_state, in_vec, out_vec):
+        out_vec.data[:] = numpy.inner(at_design.data, in_vec.data) + at_state.data[:]
+
+    def multiply_dRdU(self, at_design, at_state, in_vec, out_vec):
         pass
 
-    def eval_ceq(self, at_design, at_state, result):
+    def multiply_dRdX_T(self, at_design, at_state, in_vec, out_vec):
+        out_vec.data[:] = numpy.inner(at_state.data, in_vec.data) + at_design.data[:]
+
+    def multiply_dRdU_T(self, at_design, at_state, in_vec, out_vec):
         pass
 
-    def multiply_jac_d(self, at_design, at_state, in_vec, out_vec):
+    def build_precond(self):
+        pass
+
+    def apply_precond(self, at_design, at_state, in_vec, out_vec):
+        return 0
+
+    def apply_precond_T(self, at_design, at_state, in_vec, out_vec):
+        return 0
+
+    def multiply_dCdX(self, at_design, at_state, in_vec, out_vec):
+        out_vec.data[:] = self.eval_obj(at_design, at_state)*numpy.sum(in_vec)
+
+    def multiply_dCdU(self, at_design, at_state, in_vec, out_vec):
+        pass
+
+    def multiply_dCdX_T(self, at_design, at_state, in_vec, out_vec):
+        out_vec.data[:] = self.eval_obj(at_design, at_state)*numpy.sum(in_vec)
+
+    def multiply_dCdU_T(self, at_design, at_state, in_vec, out_vec):
+        pass
+
+    def eval_dFdX(self, at_design, at_state, store_here):
+        store_here.data[:] =
+
+    def eval_dFdU(self, at_design, at_state, store_here):
+        pass
+
+    def init_design(self, store_here):
+        pass
+
+    def solve_nonlinear(self, at_design, result):
+        return 0
+
+    def solve_linear(self, at_design, at_state, rhs_vec, rel_tol, result):
+        return 0
+
+    def solve_adjoint(self, at_design, at_state, rhs_vec, rel_tol, result):
+        return 0
+
+    def user_info(self, curr_design, curr_state, curr_adj, curr_dual, num_iter):
+        pass
