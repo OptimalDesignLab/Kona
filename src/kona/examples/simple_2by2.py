1ab6f26de991a46781cf0f071d9526c49adf0c4f
justin gray <justin.s.gray@gmail.com>
2015-04-08 17:48:19 -0400

    simple2x2 ready for quasinewton testing

--- src/kona/examples/simple_2by2.py
+++ src/kona/examples/simple_2by2.py
@@ -9,55 +9,49 @@ class Simple2x2(UserSolver):
     def __init__(self):
         super(Simple2x2, self).__init__(4,2,0)
 
+        self.dRdX = -1*numpy.eye(2)
+        self.dRdU = numpy.array([[1,1],[0,1]])
+
     def eval_obj(self, at_design, at_state, ):
-        return 2*numpy.sum(at_design.data**2) + numpy.sum(at_state.data)
+        return numpy.sum(at_state.data**2) + (at_design.data[0]-3)**2
 
     def eval_residual(self, at_design, at_state, store_here):
         p = at_design.data
-        A = p.reshape(2,2)
-        store_here.data[:] = A.dot(at_state.data) - np.array([1,2])
+        u = at_state.data
+        store_here.data[:] = self.dRdU.dot(u) - p
 
     def multiply_dRdX(self, at_design, at_state, in_vec, out_vec):
-        u = at_state.data
-        dRdX = np.zeros((4,2))
-        dRdX[0][:2] = u
-        dRdX[1][2:] = u
-        out_vec.data = dRdX.dot(in_vec.data)
+        out_vec.data = self.dRdX.dot(in_vec.data)
 
     def multiply_dRdU(self, at_design, at_state, in_vec, out_vec):
-        X = at_design.data
-        dRdU = X.reshape(2,2)
-        out_vec.data[:] = dRdU.dot(in_vec.data)
+        out_vec.data = self.dRdU.dot(in_vec.data)
 
     def multiply_dRdX_T(self, at_design, at_state, in_vec, out_vec):
-        u = at_state.data
-        dRdX = np.zeros((4,2))
-        dRdX[0][:2] = u
-        dRdX[1][2:] = u
-        out_vec.data = dRdX.T.dot(in_vec.data)
+        out_vec.data = self.dRdX.T.dot(in_vec.data)
 
     def multiply_dRdU_T(self, at_design, at_state, in_vec, out_vec):
-        X = at_design.data
-        dRdU = X.reshape(2,2)
-        out_vec.data[:] = dRdU.T.dot(in_vec.data)
+        out_vec.data = self.dRdU.T.dot(in_vec.data)
 
     def eval_dFdX(self, at_design, at_state, store_here):
-        store_here.data[:] = numpy.sum(at_state.data)*at_design.data[:]
+        X = at_design.data
+        store_here.data = numpy.array([2*X[0], 2*X[1]])
 
     def eval_dFdU(self, at_design, at_state, store_here):
-        pass
+        U = at_state.data
+        store_here.data = numpy.array([2*(U[0]), 0])
 
     def init_design(self, store_here):
-        store_here.data = 4.*numpy.ones(self.num_primal)
+        store_here.data = numpy.array([10., 10.])
 
     def solve_nonlinear(self, at_design, result):
-        return 0
+        result.data = numpy.linalg.solve(self.dRdU, at_design.data)
 
     def solve_linear(self, at_design, at_state, rhs_vec, rel_tol, result):
-        return 0
+        X = at_design.data
+        result.data = numpy.linalg.solve(dRdU, rhs_vec.data)
 
     def solve_adjoint(self, at_design, at_state, rhs_vec, rel_tol, result):
-        return 0
+        result.data = numpy.linalg.solve(self.dRdU.T, rhs_vec.data)
 
-    def user_info(self, curr_design, curr_state, curr_adj, curr_dual, num_iter):
-        pass
+    # def user_info(self, curr_design, curr_state, curr_adj, curr_dual, num_iter):
+    #     pass
