b6acabd28cd75c5fe76b4c0e32344b47ff806824
justin gray <justin.s.gray@gmail.com>
2015-04-08 10:05:47 -0400

    cleaning up more design to primal

--- src/kona/test/test_kona_memory.py
+++ src/kona/test/test_kona_memory.py
@@ -4,7 +4,7 @@ import unittest
 import numpy as np
 
 from kona.linalg.memory import KonaMemory
-from kona.linalg.vectors.common import DesignVector
+from kona.linalg.vectors.common import PrimalVector
 from kona.user.user_solver import UserSolver
 
 class VectorFactoryTestCase(unittest.TestCase):
@@ -12,23 +12,23 @@ class VectorFactoryTestCase(unittest.TestCase):
     def test_generate(self):
         solver = UserSolver()
         km = KonaMemory(solver)
-        vf = km.design_factory
+        vf = km.primal_factory
 
         vf.request_num_vectors(10)
         vf.request_num_vectors(2)
         km.allocate_memory()
-        self.assertEqual(len(km.vector_stack[DesignVector]), 12)
+        self.assertEqual(len(km.vector_stack[PrimalVector]), 12)
 
         vec0 = vf.generate()
         vec1 = vf.generate()
         vec2 = vf.generate()
 
-        self.assertEqual(len(km.vector_stack[DesignVector]), 9)
+        self.assertEqual(len(km.vector_stack[PrimalVector]), 9)
 
         del vec0, vec1, vec2
         # have to make sure things get cleaned up here to avoid race conditions
         gc.collect()
-        self.assertEqual(len(km.vector_stack[DesignVector]), 12)
+        self.assertEqual(len(km.vector_stack[PrimalVector]), 12)
 
 
 if __name__ == "__main__":
