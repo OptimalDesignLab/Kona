4a5e10dd981f49d5c0e5ddcc933048695b457432
justin gray <justin.s.gray@gmail.com>
2015-04-08 14:09:05 -0400

    added custom error msg for bad optns

---
+++ src/kona/test/test_kona_error.py
@@ -0,0 +1,22 @@
+import unittest
+
+from kona.util import BadKonaOption
+
+
+class KonaOptionTestCase(unittest.TestCase):
+
+    def test_err_msg(self):
+        optns = {
+            'quasi_newton': {'type': 2}
+        }
+        try:
+            raise BadKonaOption(optns, ('quasi_newton', 'type'))
+        except BadKonaOption as err:
+            self.assertEqual(str(err), "Invalid Kona option: optns['quasi_newton']['type'] = 2")
+        else:
+            self.fail('BadKonaOption expected')
+
+
+if __name__ == "__main__":
+
+    unittest.main()
