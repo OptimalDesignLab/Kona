057baeed055f25ccba6434262a51f2d310053887
justin gray <justin.s.gray@gmail.com>
2015-04-09 19:29:22 -0400

    almost have a test working

--- src/kona/algorithms/util/linesearch.py
+++ src/kona/algorithms/util/linesearch.py
@@ -5,7 +5,7 @@ class StrongWolfe(object):
     pass
 
 class BackTracking(object):
-    
+
     def __init__(self):
         self.alpha_init = 1.0
         self.alpha_min = 1e-4
@@ -14,42 +14,30 @@ class BackTracking(object):
         self.curv_cond = 5e-1
         self.max_iter = 10
         self.p_dot_dfdx_ = 0.0
+        self.merit_function = None
 
-    def set_initial_step_length(self, alpha_init):
-        assert 0. < alpha_init <= 1.
-        self.alpha_init = alpha_init
+    def find_step_length(self):
 
-    def set_minimum_step(self, alpha_min):
-        self.alpha_min = alpha_min
+        if not (0 < self.alpha_init <= 1):
+            raise ValueError('alpha_init must be 0 < alpha_init <=1')
 
-    def set_rdtn_factor(self, rdtn_factor):
-        self.rdtn_factor = rdtn_factor
-
-    def set_conv(self, decr_cond, curv_cond, max_iter):
-        self.decr_cond = decr_cond
-        self.curv_cond = curv_cond
-        self.max_iter = max_iter
-
-    def set_search_dot_grad(self, p_dot_grad):
-        self.p_dot_dfdx_ = p_dot_grad
         if self.p_dot_dfdx_ > 0.0:
             raise Exception('LineSearch(SetSearchDotGrad):' +
                             'search direction is not a descent direction')
 
-    def set_merit_function(self, merit_function):
-        self.merit_function = merit_function
+        if not self.merit_function:
+            raise ValueError('merit_function can not be None')
 
-    def find_step_length(self):
-        func = self.merit_function.eval_func
+        func = self.merit_function
 
-        alpha = 1.0
+        alpha = self.alpha_init
         f_init = func(0.0)
         f = func(alpha)
 
-        counter = 0
-        while counter < max_iter:
+        for i in xrange(self.max_iter):
+            print "foo", i, alpha, f_init
             if f <= f_init + self.decr_cond * alpha * self.p_dot_dfdx_:
-                return alpha
+                return alpha, i+1
             else:
                 alpha *= self.rdtn_factor
                 f = func(alpha)
