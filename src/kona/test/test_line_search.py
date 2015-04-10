77c11ee269bbcfd415fb731a64d6a92524002bec
justin gray <justin.s.gray@gmail.com>
2015-04-09 22:24:15 -0400

    backtracking is tested

--- src/kona/test/test_line_search.py
+++ src/kona/test/test_line_search.py
@@ -60,8 +60,7 @@ class BackTrackingTestCase(unittest.TestCase):
         alpha, n_iter = self.bt.find_step_length()
 
         self.assertEqual(n_iter, 1)
-
-        #cheating to get value of function at 2.2, 2.2
+        self.assertEqual(alpha, .09)
 
 
     def test_stops_after_multiple_iter(self):
@@ -76,19 +75,85 @@ class BackTrackingTestCase(unittest.TestCase):
         self.merit.reset(search_dir, at_design)
 
         self.bt.merit_function = self.merit.eval_func
-        self.bt.alpha_init = .3 #should evaluate 2.5, 2.5
+        self.bt.alpha_init = 1
+        # have to raise this to get it to take multiple steps
+        self.bt.rdtn_factor = .95
+        alpha, n_iter = self.bt.find_step_length()
+
+        self.assertEqual(n_iter, 3)
+
+
+    def test_from_running_other_way(self):
+        '''Check that it works when the search direction sign is flips'''
+
+        search_dir = self.pf.generate()
+        search_dir._data.data[:] = [4.25,0]
+
+        at_design = self.pf.generate()
+        at_design.equals(-2)
+
+        at_state = self.sf.generate()
+        at_state.equals_primal_solution(at_design)
+
+        state_work = self.sf.generate()
+
+        adjoint = self.sf.generate()
+        adjoint.equals_adjoint_solution(at_design, at_state, state_work)
+
+        primal_work = self.pf.generate()
+
+        dfdx = self.pf.generate()
+        dfdx.equals_total_gradient(at_design, at_state, adjoint, primal_work)
+        self.bt.p_dot_dfdx = dfdx.inner(search_dir)
+
+        self.merit.reset(search_dir, at_design)
+
+        self.bt.merit_function = self.merit.eval_func
+        self.bt.alpha_init = 1 #should evaluate 2.5, 2.5
         alpha, n_iter = self.bt.find_step_length()
 
         self.assertEqual(n_iter, 1)
+        self.assertEqual(alpha, .3)
 
+    def test_bad_search_direction(self):
 
-    def test_from_left_to_right(self):
-        '''Assuming your first guess viloates sufficient decrease condition'''
-        # self.fail("untested")
+        search_dir = self.pf.generate()
+        search_dir._data.data[:] = [-1,0]
 
-    def test_from_right_to_left(self):
-        '''Assuming your first guess viloates sufficient decrease condition'''
-        # self.fail("untested")
+        at_design = self.pf.generate()
+        at_design.equals(1)
+
+        self.merit.reset(search_dir, at_design)
+
+        self.bt.merit_function = self.merit.eval_func
+        self.bt.alpha_init = .3 #should evaluate 2.5, 2.5
+
+        self.bt.p_dot_dfdx *= -1
+        try:
+            alpha, n_iter = self.bt.find_step_length()
+        except ValueError as err:
+            self.assertEqual(str(err), 'search direction is not a descent direction')
+        else:
+            self.fail('ValueError expected')
+
+    def test_no_merit_function(self):
+        self.bt.merit_function = None
+        try:
+            alpha, n_iter = self.bt.find_step_length()
+        except ValueError as err:
+            self.assertEqual(str(err), 'merit_function can not be None')
+        else:
+            self.fail('ValueError expected')
+
+    def test_bad_alpha_init(self):
+        self.bt.alpha_init = 1e6
+        try:
+            alpha, n_iter = self.bt.find_step_length()
+        except ValueError as err:
+            self.assertEqual(str(err), 'alpha_init must be 0 < alpha_init <=1')
+        else:
+            self.fail('ValueError expected')
+
 
 if __name__ == "__main__":
 
