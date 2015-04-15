c8915610eb0c9edb1b88588f5cff3edf047cc285
justin gray <justin.s.gray@gmail.com>
2015-04-14 21:38:59 -0400

    equals works

--- src/kona/test/test_reduced_kkt_vector.py
+++ src/kona/test/test_reduced_kkt_vector.py
@@ -9,7 +9,7 @@ from kona.linalg.vectors.composite import ReducedKKTVector
 class ReducedKKTVectorTestCase(unittest.TestCase):
 
     def setUp(self):
-        solver = DummySolver(10, 10, 10)
+        solver = DummySolver(10, 0, 5)
         self.km = km = KonaMemory(solver)
 
         km.primal_factory.request_num_vectors(3)
@@ -19,22 +19,51 @@ class ReducedKKTVectorTestCase(unittest.TestCase):
         #can't create bear KonaVectors because the memory manager doesn't
         # like them, so I'll just use the PrimalVector to test the
         # KonaVectorMethods
-        self.pv = km.primal_factory.generate()
-        self.dv = km.dual_factory.generate()
+        self.pv1 = km.primal_factory.generate()
+        self.dv1 = km.dual_factory.generate()
 
-        self.rkkt_vec = ReducedKKTVector(km, self.pv, self.dv)
+        self.pv2 = km.primal_factory.generate()
+        self.dv2 = km.dual_factory.generate()
+
+        self.rkkt_vec1 = ReducedKKTVector(km, self.pv1, self.dv1)
+        self.rkkt_vec2 = ReducedKKTVector(km, self.pv2, self.dv2)
 
     def test_check_type(self):
 
         try:
-            self.rkkt_vec._check_type(self.pv)
+            self.rkkt_vec1._check_type(self.pv1)
         except TypeError as err:
             self.assertEquals(str(err), "ReducedKKTVector() >> Wrong vector type. Must be <class 'kona.linalg.vectors.composite.ReducedKKTVector'>")
         else:
             self.fail('TypeError expected')
 
+    def test_bad_init_args(self):
+
+        try:
+            ReducedKKTVector(self.km, self.dv1, self.dv1)
+        except TypeError as err:
+            self.assertEqual(str(err), 'ReducedKKTVector() >> Unidentified design vector.' )
+        else:
+            self.fail('TypeError expected')
+
+        try:
+            ReducedKKTVector(self.km, self.pv1, self.pv1)
+        except TypeError as err:
+            self.assertEqual(str(err), 'ReducedKKTVector() >> Unidentified dual vector.' )
+        else:
+            self.fail('TypeError expected')
+
     def test_equals(self):
-        pass
+        self.pv1._data.data = 2*np.ones(10)
+        self.dv1._data.data = 3*np.ones(5)
+
+        self.rkkt_vec2.equals(self.rkkt_vec1)
+
+        err = self.dv2._data.data - self.dv1._data.data
+        self.assertEquals(np.linalg.norm(err), 0)
+
+        err = self.pv2._data.data - self.pv1._data.data
+        self.assertEquals(np.linalg.norm(err), 0)
 
     def test_plus(self):
         pass
