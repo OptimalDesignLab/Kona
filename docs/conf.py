8200fec14bf0c3584a702dda744dade725472c7f
Alp Dener <alp@dener.me>
2015-04-19 14:04:41 -0400

    Theme options for Read The Docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -106,15 +107,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'scipy'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'scipy'
+    html_theme_path = [os.path.join('scipy-sphinx-theme','_theme')]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [os.path.join('scipy-sphinx-theme','_theme')]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
