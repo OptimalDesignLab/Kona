3c96358e33b1bb1a2db1fd8e08ed2b436d8c30c4
Alp Dener <alp@dener.me>
2016-01-04 16:37:46 -0800

    Import errors fixed

--- src/kona/examples/__init__.py
+++ src/kona/examples/__init__.py
@@ -5,8 +5,3 @@ from simple_2by2 import Simple2x2
 from constrained_2by2 import Constrained2x2
 from spiral import Spiral
 from sellar import Sellar
-
-try:
-    from mdo_idf import ScalableIDF
-except ImportError:
-    print 'WARNING: PyAMG not installed. ScalableIDF example will not work.'
