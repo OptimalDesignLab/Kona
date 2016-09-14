cb8999df7d699770f32106afca7a448a1ecbf3cd
Alp Dener <alp@dener.me>
2016-09-13 22:33:31 -0400

    SphereConstrained problem updated to new UserSolver interface.

--- src/kona/examples/sphere_constrained.py
+++ src/kona/examples/sphere_constrained.py
@@ -1,53 +1,94 @@
+import numpy as np
+
 from kona.user import UserSolver
 
 class SphereConstrained(UserSolver):
 
     def __init__(self, init_x=[0.51, 0.52, 0.53], ineq=False):
-        super(SphereConstrained, self).__init__(3, 0, 1)
         self.ineq = ineq
-        self.init_x = init_x
+        if not self.ineq:
+            super(SphereConstrained, self).__init__(
+                num_design=3,
+                num_state=0,
+                num_eq=1,
+                num_ineq=0)
+        else:
+            super(SphereConstrained, self).__init__(
+                num_design=3,
+                num_state=0,
+                num_eq=0,
+                num_ineq=1)
+        self.init_x = np.array(init_x)
 
     def eval_obj(self, at_design, at_state):
-        return at_design.data[0] + at_design.data[1] + at_design.data[2]
+        return at_design[0] + at_design[1] + at_design[2]
 
     def eval_residual(self, at_design, at_state, store_here):
         store_here.data[:] = 0.
 
-    def eval_constraints(self, at_design, at_state, store_here):
-        x = at_design.data[0]
-        y = at_design.data[1]
-        z = at_design.data[2]
-        store_here.data[0] = 3 - (x**2 + y**2 + z**2)
+    def eval_cnstr(self, at_design, at_state):
+        x = at_design[0]
+        y = at_design[1]
+        z = at_design[2]
+        return np.array([3 - (x ** 2 + y ** 2 + z ** 2)])
 
-    def multiply_dCdX(self, at_design, at_state, in_vec, out_vec):
-        x = at_design.data[0]
-        y = at_design.data[1]
-        z = at_design.data[2]
-        out_vec.data[0] = -2*(x*in_vec.data[0] +
-                              y*in_vec.data[1] +
-                              z*in_vec.data[2])
+    def eval_eq_cnstr(self, at_design, at_state):
+        if not self.ineq:
+            return self.eval_cnstr(at_design, at_state)
+        else:
+            return np.zeros(self.num_eq)
 
-    def multiply_dCdX_T(self, at_design, at_state, in_vec, out_vec):
-        x = at_design.data[0]
-        y = at_design.data[1]
-        z = at_design.data[2]
-        out_vec.data[0] = -2*x*in_vec.data[0]
-        out_vec.data[1] = -2*y*in_vec.data[0]
-        out_vec.data[2] = -2*z*in_vec.data[0]
+    def eval_ineq_cnstr(self, at_design, at_state):
+        if self.ineq:
+            return self.eval_cnstr(at_design, at_state)
+        else:
+            return np.zeros(self.num_ineq)
 
-    def eval_dFdX(self, at_design, at_state, store_here):
-        store_here.data[:] = 1.0
+    def multiply_dCdX(self, at_design, at_state, in_vec):
+        x = at_design[0]
+        y = at_design[1]
+        z = at_design[2]
+        return np.array([-2 * (x * in_vec[0] + y * in_vec[1] + z * in_vec[2])])
+
+    def multiply_dCdX_T(self, at_design, at_state, in_vec):
+        x = at_design[0]
+        y = at_design[1]
+        z = at_design[2]
+        out = np.zeros(self.num_design)
+        out[0] = -2 * x * in_vec[0]
+        out[1] = -2 * y * in_vec[0]
+        out[2] = -2 * z * in_vec[0]
+        return out
+
+    def multiply_dCEQdX(self, at_design, at_state, in_vec):
+        if not self.ineq:
+            return self.multiply_dCdX(at_design, at_state, in_vec)
+        else:
+            return np.zeros(self.num_eq)
+
+    def multiply_dCEQdX_T(self, at_design, at_state, in_vec):
+        if not self.ineq:
+            return self.multiply_dCdX_T(at_design, at_state, in_vec)
+        else:
+            return np.zeros(self.num_design)
+
+    def multiply_dCINdX(self, at_design, at_state, in_vec):
+        if self.ineq:
+            return self.multiply_dCdX(at_design, at_state, in_vec)
+        else:
+            return np.zeros(self.num_eq)
+
+    def multiply_dCINdX_T(self, at_design, at_state, in_vec):
+        if self.ineq:
+            return self.multiply_dCdX_T(at_design, at_state, in_vec)
+        else:
+            return np.zeros(self.num_design)
+
+    def eval_dFdX(self, at_design, at_state):
+        return np.ones(self.num_design)
 
     def eval_dFdU(self, at_design, at_state, store_here):
         store_here.data[:] = 0.0
 
-    def init_design(self, store_here):
-        store_here.data[0] = self.init_x[0]
-        store_here.data[1] = self.init_x[1]
-        store_here.data[2] = self.init_x[2]
-
-    def restrict_dual(self, dual_vector):
-        if self.ineq:
-            pass
-        else:
-            dual_vector.data[:] = 0.
+    def init_design(self):
+        return self.init_x
