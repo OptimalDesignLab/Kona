51ed5843e6e8aa61fcaff6c3faaf4f1e528477ee
Alp Dener <alp@dener.me>
2016-09-13 21:42:12 -0400

    ExponentialConstrained updated to the new UserSolver interface.

--- src/kona/examples/exponential_constrained.py
+++ src/kona/examples/exponential_constrained.py
@@ -5,41 +5,39 @@ from kona.user import UserSolver
 class ExponentialConstrained(UserSolver):
 
     def __init__(self, init_x=[1., 1.]):
-        super(ExponentialConstrained, self).__init__(2, 0, 1)
-        self.init_x = init_x
+        super(ExponentialConstrained, self).__init__(
+            num_design=2,
+            num_state=0,
+            num_eq=1,
+            num_ineq=0)
+        self.init_x = np.array(init_x)
 
     def eval_obj(self, at_design, at_state):
-        x = at_design.data[0]
-        y = at_design.data[1]
+        x = at_design[0]
+        y = at_design[1]
         return x + y**2
 
     def eval_residual(self, at_design, at_state, store_here):
         pass
 
-    def eval_constraints(self, at_design, at_state, store_here):
-        x = at_design.data[0]
-        store_here.data[0] = np.exp(x) - 1.
+    def eval_eq_cnstr(self, at_design, at_state):
+        x = at_design[0]
+        return np.array([np.exp(x) - 1.])
 
-    def multiply_dCdX(self, at_design, at_state, in_vec, out_vec):
-        x = at_design.data[0]
-        out_vec.data[0] = np.exp(x)*in_vec.data[0]
+    def multiply_dCEQdX(self, at_design, at_state, in_vec):
+        x = at_design[0]
+        return np.array([np.exp(x)*in_vec[0]])
 
-    def multiply_dCdX_T(self, at_design, at_state, in_vec, out_vec):
-        x = at_design.data[0]
-        out_vec.data[0] = np.exp(x)*in_vec.data[0]
-        out_vec.data[1] = 0.
+    def multiply_dCEQdX_T(self, at_design, at_state, in_vec):
+        x = at_design[0]
+        return np.array([np.exp(x)*in_vec[0], 0.])
 
-    def eval_dFdX(self, at_design, at_state, store_here):
-        y = at_design.data[1]
-        store_here.data[0] = 1.
-        store_here.data[1] = 2*y
+    def eval_dFdX(self, at_design, at_state):
+        y = at_design[1]
+        return np.array([1., 2*y])
 
     def eval_dFdU(self, at_design, at_state, store_here):
         store_here.data[:] = 0.0
 
-    def init_design(self, store_here):
-        store_here.data[0] = self.init_x[0]
-        store_here.data[1] = self.init_x[1]
-
-    def restrict_dual(self, dual_vector):
-        pass
+    def init_design(self):
+        return self.init_x
