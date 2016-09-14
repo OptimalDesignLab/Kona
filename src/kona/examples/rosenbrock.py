defc9a87f52d28b19cff1f5913549b679663acc2
Alp Dener <alp@dener.me>
2016-09-13 21:44:46 -0400

    Rosenbrock problem updated to new UserSolver interface.

--- src/kona/examples/rosenbrock.py
+++ src/kona/examples/rosenbrock.py
@@ -4,15 +4,19 @@ from kona.user import UserSolver
 
 class Rosenbrock(UserSolver):
 
-    def __init__(self, num_design):
-        super(Rosenbrock, self).__init__(num_design,0,0)
+    def __init__(self, ndv):
+        super(Rosenbrock, self).__init__(
+            num_design=dv,
+            num_state=0,
+            num_eq=0,
+            num_ineq=0)
 
     def eval_obj(self, at_design, at_state):
-        x = at_design.data
+        x = at_design
         return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
-    def eval_dFdX(self, at_design, at_state, store_here):
-        x = at_design.data
+    def eval_dFdX(self, at_design, at_state):
+        x = at_design
         xm = x[1:-1]
         xm_m1 = x[:-2]
         xm_p1 = x[2:]
@@ -20,7 +24,7 @@ class Rosenbrock(UserSolver):
         der[1:-1] = 200*(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
         der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
         der[-1] = 200*(x[-1]-x[-2]**2)
-        store_here.data[:] = der[:]
+        return der
 
-    def init_design(self, store_here):
-        store_here.data[:] = -1.
+    def init_design(self):
+        return -numpy.ones(self.num_design)
