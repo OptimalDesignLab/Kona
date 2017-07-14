2f4eee5cf3d2394d6aad493e574946e6696f1a57
Alp Dener <alp.dener@gmail.com>
2017-07-14 11:09:55 -0400

    Split off composite-step work into its own branch

--- src/kona/test/test_equality_composite_step.py
+++ src/kona/test/test_equality_composite_step.py
@@ -7,66 +7,62 @@ from kona.examples import SphereConstrained
 
 class EqualityCompositeStepTestCase(unittest.TestCase):
 
-    def test_dummy(self):
-        '''CompositeStepRSNK placeholder (not working!)'''
-        self.failUnless('Untested')
-
-    # def test_with_simple_constrained(self):
-    #
-    #     solver = SphereConstrained(
-    #         ineq=False)
-    #
-    #     optns = {
-    #         'info_file' : 'kona_info.dat',
-    #         'max_iter' : 50,
-    #         'opt_tol' : 1e-5,
-    #         'feas_tol' : 1e-5,
-    #         'globalization' : 'linesearch',
-    #
-    #         'trust' : {
-    #             'init_radius' : 4.0,
-    #             'max_radius' : 1.0,
-    #             'min_radius' : 1e-4,
-    #         },
-    #
-    #         'penalty' : {
-    #             'mu_init' : 1.0,
-    #             'mu_pow' : 1e-8,
-    #         },
-    #
-    #         'composite-step' : {
-    #             'normal-step' : {
-    #                 'precond'     : None,
-    #                 'lanczos_size': 1,
-    #                 'use_gcrot'   : True,
-    #                 'out_file'    : 'kona_normal_krylov.dat',
-    #                 'max_inner'   : 10,
-    #                 'max_outer'   : 10,
-    #                 'max_recycle' : 10,
-    #                 'max_matvec'  : 100,
-    #                 'check_res'   : True,
-    #                 'rel_tol'     : 1e-5,
-    #                 'abs_tol'     : 1e-8,
-    #             },
-    #             'tangent-step' : {
-    #                 'out_file'    : 'kona_tangent_krylov.dat',
-    #                 'max_iter'    : 50,
-    #                 'check_res'   : True,
-    #                 'rel_tol'     : 1e-5,
-    #                 'abs_tol'     : 1e-8,
-    #             }
-    #         },
-    #     }
-    #
-    #     algorithm = CompositeStepRSNK
-    #     optimizer = Optimizer(solver, algorithm, optns)
-    #     optimizer.solve()
-    #
-    #     print solver.curr_design
-    #
-    #     expected = -1.*numpy.ones(solver.num_primal)
-    #     diff = abs(solver.curr_design - expected)
-    #     self.assertTrue(max(diff) < 1e-4)
+    def test_with_simple_constrained(self):
+        '''CompositeStepRSNK test with Sphere problem'''
+        solver = SphereConstrained(
+            ineq=False)
+    
+        optns = {
+            'info_file' : 'kona_info.dat',
+            'max_iter' : 50,
+            'opt_tol' : 1e-5,
+            'feas_tol' : 1e-5,
+            'globalization' : 'linesearch',
+    
+            'trust' : {
+                'init_radius' : 4.0,
+                'max_radius' : 1.0,
+                'min_radius' : 1e-4,
+            },
+    
+            'penalty' : {
+                'mu_init' : 1.0,
+                'mu_pow' : 1e-8,
+            },
+    
+            'composite-step' : {
+                'normal-step' : {
+                    'precond'     : None,
+                    'lanczos_size': 1,
+                    'use_gcrot'   : True,
+                    'out_file'    : 'kona_normal_krylov.dat',
+                    'max_inner'   : 10,
+                    'max_outer'   : 10,
+                    'max_recycle' : 10,
+                    'max_matvec'  : 100,
+                    'check_res'   : True,
+                    'rel_tol'     : 1e-5,
+                    'abs_tol'     : 1e-8,
+                },
+                'tangent-step' : {
+                    'out_file'    : 'kona_tangent_krylov.dat',
+                    'max_iter'    : 50,
+                    'check_res'   : True,
+                    'rel_tol'     : 1e-5,
+                    'abs_tol'     : 1e-8,
+                }
+            },
+        }
+    
+        algorithm = CompositeStepRSNK
+        optimizer = Optimizer(solver, algorithm, optns)
+        optimizer.solve()
+    
+        print solver.curr_design
+    
+        expected = -1.*numpy.ones(solver.num_primal)
+        diff = abs(solver.curr_design - expected)
+        self.assertTrue(max(diff) < 1e-4)
 
 if __name__ == "__main__":
     unittest.main()
