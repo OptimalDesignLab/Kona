1d02d8544477e2212ee0e4c8e9b56b32fb580d5e
Jason Hicken <hickej2@rpi.edu>
2017-07-19 22:04:31 -0400

    finished and tested draft version of ApproxSchur preconditioner

--- src/kona/linalg/matrices/preconds/schur.py
+++ src/kona/linalg/matrices/preconds/schur.py
@@ -1,49 +1,82 @@
 import numpy as np
+from kona.linalg.vectors.composite import CompositeFactory, PrimalDualVector, CompositeDualVector
 from kona.linalg.matrices.hessian.basic import BaseHessian
+from kona.linalg.matrices.hessian import TotalConstraintJacobian
+from kona.linalg.matrices.preconds.low_rank_svd import LowRankSVD
 
 class ApproxSchur(BaseHessian):
     """
-    Uses an approximate SVD to approximate the Schur complement of the primal-dual matrix.
+    Uses an approximate SVD to estimate the inverse Schur complement of the primal-dual matrix.
+    Specifically, this preconditioner forms an approximate inverse of the augmented matrix
 
-    Parameters
-    ----------
+    .. math::
+        \\begin{bmatrix} I && -A^T \\\\ -A && 0 \\end{bmatrix}
 
+    where :math:`A` is the total constraint Jacobian.  The preconditioner is based on an LU
+    decomposition with the Schur complement approximated by a low-rank update to a diagonal matrix.
+    The action of the preconditioner on a vector :math:`[u_p^T u_v^T]^T` is given by
+
+    .. math::
+        \\begin{bmatrix} u_p - \\tilde{A}^T (\\tilde{A}\\tilde{A}^T)^{-1}(\\tilde{A}u_p - u_d) \\\\
+        - (\\tilde{A}\\tilde{A}^T)^{-1}(\\tilde{A}u_p - u_d) \\end{bmatrix}
+
+    where :math:`\\tilde{A}` is the low-rank approximation to the constraint Jacobian, and
+    :math:`(\\tilde{A}\\tilde{A}^{T})^{-1}` is found using a pseudo-inverse-like approximation: see
+    LowRankSVD.inv_schur_prod for further details on this.
 
     Attributes
     ----------
-    subspace_size : int
-        Number of iterations in the Lanczos algorithm, and the number of singular values
-        approximated by the decomposition.
+    dCdX : TotalConstraintJacobian
+        Used in Lanczos bi-diagonalization to find low-rank SVD
+    dCdX_approx : LowRankSVD
+        Used to approximate the inverse of (dCdX)*(dCdX^T)
     """
-    def __init__(self,  vector_factories, optns=None):
+    def __init__(self, vector_factories, optns=None):
         super(ApproxSchur, self).__init__(vector_factories, optns)
 
-        # request vectors needed (work)
-        dual_work = None
-        if self.eq_factory is not None:
-            self.eq_factory.request_num_vectors(1)
-        if self.ineq_factory is not None:
-            self.ineq_factory.request_num_vectors(1)
+        # set-up the dual factory
+        self.dual_factory = None
+        if self.eq_factory is not None and self.ineq_factory is not None:
+            self.dual_factory = CompositeFactory(self.eq_factory._memory, CompositeDualVector)
+        elif self.eq_factory is not None:
+            self.dual_factory = self.eq_factory
+        else:
+            self.dual_factory = self.ineq_factory
+
+        # request vectors needed (dual_work)
+        self.dual_work = None
+        if self.dual_factory is not None:
+            self.dual_factory.request_num_vectors(1)
 
         # initialize the total constraint jacobian block
         self.dCdX = TotalConstraintJacobian(vector_factories)
 
-        # Questions for Alp:
-        # 1) LowRankSVD needs a rev_factory for rectangular matrices; what if CompositeDualVector?
-        # 2) Would it be safe to implement a CompositeDualVector factory?
-        # 3) Looks like LowRankSVD assumes BaseVectors, correct?
-
         # initialize the low-rank SVD
-        # Will these definitions persist!!!!
         def fwd_mat_vec(in_vec, out_vec):
             self.dCdX.product(in_vec, out_vec)
+
         def rev_mat_vec(in_vec, out_vec):
             self.dCdX.T.product(in_vec, out_vec)
-        self.Schur_inv = LowRankSVD(fwd_mat_vec, self.primal_factory,
-                                    rev_mat_vec, [self.eq_factory, self.ineq_factory])
+
+        self.dCdX_approx = LowRankSVD(fwd_mat_vec, self.primal_factory,
+                                      rev_mat_vec, self.dual_factory, optns)
+
+        self._allocated = False
 
     def linearize(self, at_primal, at_state, scale=1.0):
-        # need to generate dual_work here
+        # store references to the evaluation point, and save the scaling of constraint terms
+        self.at_design = at_primal
+        self.at_state = at_state
+        self.scale = scale
+
+        # get the total constraint Jacobian ready, and then use it in Lanczos bi-diagonalization
+        self.dCdX.linearize(self.at_design, self.at_state, scale=self.scale)
+        self.dCdX_approx.linearize()
+
+        # check if dual_work needs to be allocated
+        if not self._allocated:
+            if self.dual_factory is not None:
+                self.dual_work = self.dual_factory.generate()
 
     def product(self, in_vec, out_vec):
         assert isinstance(in_vec, PrimalDualVector), \
@@ -58,13 +91,15 @@ class ApproxSchur(BaseHessian):
         out_dual = out_vec.get_dual()
         if dual_work is not None:
             # Step 1: w <-- A*u_p + u_d
-            self.dCdX.product(in_primal, dual_work)
+            self.dCdX_approx.approx_fwd_prod(in_primal, dual_work)
             dual_work.plus(in_dual)
-            # Step 2: v_d <-- (A*A^T)^-1*w
-            self.Schur_inv.inv_schur_product(dual_work, out_dual)
-            # Step 3: v_p <-- u_p - A^T*v_d
-            self.dCdX.T.product(out_dual, out_primal)
+            # Step 2: v_d <-- -(A*A^T)^-1*w
+            self.dCdX_approx.inv_schur_prod(dual_work, out_dual)
+            out_dual.times(-1.)
+            # Step 3: v_p <-- A^T*v_d
+            self.dCdX_approx.approx_rev_prod(out_dual, out_primal)
         else:
             # no preconditioning for unconstrained case
             out_primal.equals(0.)
-        out_primal.equals_ax_p_by(1., in_primal, -1., out_primal)
+        # v_p <-- v_p + u_p
+        out_primal.equals_ax_p_by(1., in_primal, 1., out_primal)
---
+++ src/kona/test/test_approx_schur.py
@@ -0,0 +1,132 @@
+import numpy as np
+import unittest
+import inspect
+import os
+
+from kona import Optimizer
+from kona.algorithms.base_algorithm import OptimizationAlgorithm
+from kona.linalg.matrices.preconds.schur import ApproxSchur
+from kona.user import UserSolver
+from kona.linalg.vectors.composite import PrimalDualVector
+# from kona.linalg.matrices.hessian.constraint_jacobian import TotalConstraintJacobian
+from kona.linalg.matrices.hessian import TotalConstraintJacobian
+
+class SolverForApproxSchur(UserSolver):
+    """A minimal class used to provided synthetic total-constraint Jacobians"""
+
+    def __init__(self, ndv=20, neq=10, nineq=0):
+        super(SolverForApproxSchur, self).__init__(
+            num_design=ndv,
+            num_state=0,
+            num_eq=neq,
+            num_ineq=nineq)
+        self.num_design = ndv
+        self.num_eq = neq
+        self.num_ineq = nineq
+        path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        self.dCdX = np.loadtxt(path+'/synthetic_jac.dat')
+
+    def multiply_dCEQdX(self, at_design, at_state, in_vec):
+        if self.num_eq > 0:
+            return np.dot(self.dCdX[0:self.num_eq,:], in_vec)
+        else:
+            return 0.0
+
+    def multiply_dCEQdX_T(self, at_design, at_state, in_vec):
+        if self.num_eq > 0:
+            return np.dot(in_vec.T, self.dCdX[0:self.num_eq,:]).T
+        else:
+            return 0.0
+
+class AlgorithmForApproxSchur(OptimizationAlgorithm,unittest.TestCase):
+
+    def __init__(self, primal_factory, state_factory,
+                 eq_factory, ineq_factory, optns=None):
+        # trigger base class initialization
+        super(AlgorithmForApproxSchur, self).__init__(
+            primal_factory, state_factory, eq_factory, ineq_factory, optns
+        )
+        # number of vectors required in solve() method
+        self.primal_factory.request_num_vectors(3)
+        self.state_factory.request_num_vectors(1)
+        if self.eq_factory is not None:
+            self.eq_factory.request_num_vectors(2)
+        if self.ineq_factory is not None:
+            self.eq_factory.request_num_vectors(2)
+        max_lanczos = 10
+        optns = {'lanczos_size': max_lanczos}
+        self.precond = ApproxSchur([primal_factory, state_factory, eq_factory, ineq_factory], optns)
+        km = self.primal_factory
+        self.num_design = km._memory.ndv
+        self.num_eq = km._memory.neq
+        self.num_ineq = km._memory.nineq
+        # algorithm also needs direct access to the Jacobian
+        self.dCdX = TotalConstraintJacobian([primal_factory, state_factory, eq_factory,
+                                             ineq_factory])
+
+    def _generate_vector(self):
+        dual_eq = None
+        dual_ineq = None
+        primal = self.primal_factory.generate()
+        if self.eq_factory is not None:
+            dual_eq = self.eq_factory.generate()
+        if self.ineq_factory is not None:
+            dual_ineq = self.ineq_factory.generate()
+        return PrimalDualVector(primal, eq_vec=dual_eq, ineq_vec=dual_ineq)
+
+    def exact_product(self, in_vec, out_vec):
+        """Apply the exact augmented matrix using constraint Jacobian from file"""
+        self.dCdX.T.product(in_vec.get_dual(), out_vec.primal)
+        out_vec.primal.equals_ax_p_by(1.0, in_vec.primal, -1.0, out_vec.primal)
+        self.dCdX.product(in_vec.primal, out_vec.get_dual())
+        out_vec.get_dual().times(-1.)
+
+    def solve(self):
+        # at_state and at_design are not used, but they need to be defined
+        at_state = self.state_factory.generate()
+        at_design = self.primal_factory.generate()
+        u_vec = self._generate_vector()
+        v_vec = self._generate_vector()
+        self.dCdX.linearize(at_design, at_state)
+        self.precond.linearize(at_design, at_state)
+        for i in xrange(20):
+            u_vec.equals(0.)
+            v_vec.equals(0.)
+            u_vec.primal.base.data[i] = 1.0
+            self.exact_product(u_vec, v_vec)
+            u_vec.equals(0.)
+            self.precond.product(v_vec, u_vec)
+            for j in xrange(20):
+                if j == i:
+                    self.assertAlmostEqual(u_vec.primal.base.data[j], 1., places=10)
+                else:
+                    self.assertAlmostEqual(u_vec.primal.base.data[j], 0., places=10)
+            for j in xrange(10):
+                self.assertAlmostEqual(u_vec.eq.base.data[j], 0., places=10)
+
+        for i in xrange(10):
+            u_vec.equals(0.)
+            v_vec.equals(0.)
+            u_vec.eq.base.data[i] = 1.0
+            self.exact_product(u_vec, v_vec)
+            u_vec.equals(0.)
+            self.precond.product(v_vec, u_vec)
+            for j in xrange(20):
+                self.assertAlmostEqual(u_vec.primal.base.data[j], 0., places=10)
+            for j in xrange(10):
+                if j == i:
+                    self.assertAlmostEqual(u_vec.eq.base.data[j], 1., places=10)
+                else:
+                    self.assertAlmostEqual(u_vec.eq.base.data[j], 0., places=10)
+
+class ApproxSchurTestCase(unittest.TestCase):
+
+    def test_ApproxSchur_eq(self):
+        '''test Approximate Schur precondition (equality only)'''
+        solver = SolverForApproxSchur(ndv=20, neq=10)
+        algorithm = AlgorithmForApproxSchur
+        optimizer = Optimizer(solver, algorithm)
+        optimizer.solve()
+
+if __name__ == "__main__":
+    unittest.main()
